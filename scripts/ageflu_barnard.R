bfebd933bb7eebd5cef27669a9c9807adedc3dc9
Alvin X. Han <hanxc@bii.a-star.edu.sg>
2018-04-11 18:15:13 +0200

    R script for Barnard's exact test

--- scripts/ageflu_barnard.R
+++ scripts/ageflu_barnard.R
@@ -2,6 +2,5 @@
 
 myArgs <- as.numeric(commandArgs(trailingOnly = TRUE))
 input <- matrix(myArgs, nrow=2, ncol=2, byrow = TRUE)
-
 banard.output <- Barnard::barnard.test(input[1,1],input[2,1],input[1,2],input[2,2])
 banard.output$p.value
