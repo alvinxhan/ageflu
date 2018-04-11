cbccd403d31b4a12b047c01b276769b44b82d32a
Alvin X. Han <hanxc@bii.a-star.edu.sg>
2018-04-11 18:15:40 +0200

    R script for G-test

--- scripts/ageflu_gtest.R
+++ scripts/ageflu_gtest.R
@@ -1,8 +1,6 @@
 # G-test of independence
 
-# Get 2x2 contingency table inputs 
 myArgs <- as.numeric(commandArgs(trailingOnly = TRUE))
 input <- matrix(myArgs, nrow=2, ncol=2, byrow = TRUE)
-
 output <- DescTools::GTest(input, correct="yates")
 output$p.value
